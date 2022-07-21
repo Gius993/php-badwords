9fba2b0218f3a38f87822060499f375c3fc40f90
Gius993 <giuseppevidili9@gmail.com>
2022-07-21 15:06:27 +0200

    stampa e lunghezza

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		//creo una variabile
+		$myTest = ("ciao sono un test su php");
+		
+		//stampo la variabile
+	?>
+	
+	<h1> <?php echo $myTest;
+			
+	?>
+	</h1>
+
+	<div>
+		la tua stringa è
+		<?php
+		//lunghezza stringa
+		echo  strlen($myTest);
+		?>
+		caratteri
+	</div>
+</body>
+</html>
